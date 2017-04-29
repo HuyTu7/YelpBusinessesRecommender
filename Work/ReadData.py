e06f089474af927982eed311cfc83c80139406a4
Your Name <you@example.com>
2017-04-28 21:54:39 -0400

    adding the reading part for reviews data

--- Work/ReadData.py
+++ Work/ReadData.py
@@ -10,23 +10,25 @@ unformated_data = unformated_data[0]
 for _dict in unformated_data:
     data_list.append(_dict)
 print(data_list[1])
-
 business=pd.DataFrame.from_dict(list(iter(data_list).iteritems()), orient='columns', dtype=None)
 print(business)
 #pd.dataframe(data_list)"""
 
 #Reading Business Data
-"""business=pd.DataFrame.from_csv("../../Data/yelp_academic_dataset_business.csv")
+business=pd.DataFrame.from_csv("../../Data/yelp_academic_dataset_business.csv")
 business.drop(['type','postal_code','latitude','longitude','review_count','attributes','address','is_open','hours','stars'],inplace=True,axis=1)
 business.index.name='neighborhood'
 business.reset_index(inplace=True)
-business.drop(business.columns[[0]],inplace=True,axis=1)"""
+business.drop(business.columns[[0]],inplace=True,axis=1)
+business=business.loc[business['state']=='NC']
 #print(business)
 
 #Reading Review Data
-review=pd.DataFrame.from_csv("../../Data/yelp_academic_dataset_review.csv")
+review=pd.DataFrame.from_csv("../../Data/review.csv")
 #business.drop(['type','postal_code','latitude','longitude','review_count','attributes','address','is_open','hours'],inplace=True,axis=1)
-business.index.name='reviewid'
-business.reset_index(inplace=True)
-business.drop(business.columns[[0,4,5,6,7,8,9,10]],inplace=True,axis=1)
-print(review)
+#business.index.name='reviewid'
+#business.reset_index(inplace=True)
+#business.drop(business.columns[[0,4,5,6,7,8,9,10]],inplace=True,axis=1)
+#print(review)
+
+business.merge(review,on='business_id',how='inner').to_csv("../../Data/merged_BR.csv")
